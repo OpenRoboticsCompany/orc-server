14e7f1f135c5fa7fae10aad1294c08251a5169a6
Dave Goehrig <dave@dloh.org>
2017-04-07 14:04:42 -0400

    fixing static lookup

--- src/orc_static.erl
+++ src/orc_static.erl
@@ -5,16 +5,8 @@
 
 -include("include/orc_http.hrl").
 
-get(Request = #request { path = Path }) ->	
-	case orc_auth:auth(Request) of 
-		#request{} ->
-			error_logger:info_msg("Authorized ~p~n", [ Path ]),
-			static_file(Request);
-		Response = #response{} ->
-			error_logger:error_msg("Authentication failure ~p~n", [ Request ]),
-			Response
-	end.
-
+get(Request = #request {}) ->	
+	static_file(Request).
 
 default_file(#request{ path = Path, headers = Headers }) ->
 	Host = proplists:get_value(<<"Host">>, Headers),
@@ -29,22 +21,10 @@ file(Path) ->
 	file:read_file(Filename).
 
 static_file(Request = #request{ path = Path }) ->
-	Content = case file(Path) of
+	case file(Path) of
 		{ ok, Bin } ->
 			Bin;
 		{ error, Error } ->
 			erro_logger:error_msg("Returning default ~p~n", [ Error ]),
 			default_file(Request)
-	end,
-	ContentLength = binary:list_to_bin(integer_to_list(byte_size(Content))),
-	#response{ 
-		socket = Request#request.socket,
-		upgrade = false,
-		status = 200,
-		protocol = <<"HTTP/1.1">>,
-		headers = [
-			{ <<"Content-Length">>, ContentLength },
-			{ <<"Content-Type">>, <<"text/html">> }
-		],
-		body = Content
-	}.
+	end.
